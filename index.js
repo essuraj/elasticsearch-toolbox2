4494fd4bd62234a985420007978aef4ebec1b2c7
Suraj <essuraj@live.in>
2017-02-19 15:30:37 +0530

    added express and build steps

---
+++ index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+var fs = require('fs-extra')
+app.use(express.static(__dirname + '/public'));
+
+fs.ensureDir("public", function (err) {
+    if(err)
+   { console.error(err)
+    throw "Stopped"}
+  try {
+    fs.copySync('scripts', 'public/scripts')
+    fs.copySync('index.html', 'public/index.html')
+    fs.copySync('src', 'public/src')
+    fs.removeSync("public/src/resources")
+    console.log("success!")
+  } catch (err) {
+    console.error(err)
+  }
+  app.listen(process.env.PORT || 3000, () => {
+    console.log("running at http://localhost:3000")
+  });
+});
